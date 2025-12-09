bc5fe094cbdee88c6b3ee960cc16cf0163239cc0
alex5169 <speedshorts89@gmail.com>
2025-12-09 19:47:44 +0800

    Reinstall react-twitter-embed for Twitter videos

---
+++ .next/static/chunks/src_app_page_tsx_70fad7a2._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_326e0e82._.js",
+  "static/chunks/_03094595._.js"
+],
+    source: "dynamic"
+});
